c42ae8a04b76b8a6ce420571a350681715534111
nonioAlber <nonio4932@gmail.com>
2025-11-14 19:54:05 +0100

    feat: add src/extractors/url_decoder.js

---
+++ src/extractors/url_decoder.js
@@ -0,0 +1,6 @@
+function decodeUrl(url) {
+    // Placeholder for decoding Google News redirect URL (if applicable)
+    return url.replace('https://news.google.com/', '');
+}
+
+module.exports = { decodeUrl };
